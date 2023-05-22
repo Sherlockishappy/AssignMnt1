001a64fd3f15c7ed899b3aab47feda430fc01929
Jingjing Zhao <JIZO@simcorp.com>
2023-05-22 16:19:11 +0200

    master change

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace AssignMnt1 {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("The weather is great!");
+            Console.WriteLine("Feature branch 1 - development 2");
             Console.ReadKey();
         }
     }
