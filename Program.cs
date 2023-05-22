ee08664e933dba8a46ef31c43ffbf0dd0bbda901
Jingjing Zhao <JIZO@simcorp.com>
2023-05-22 15:27:31 +0200

    Assignment 1 V1

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignMnt1 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("The weather is great!");
+            Console.ReadKey();
+        }
+    }
+}
