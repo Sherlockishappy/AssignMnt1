b0b470c538d77f82a890578ffb3091d0c70b09c2
Jingjing Zhao <JIZO@simcorp.com>
2023-05-22 16:41:18 +0200

    Revert "master change"
    This reverts commit 001a64fd3f15c7ed899b3aab47feda430fc01929.

--- Program.cs
+++ Program.cs
@@ -8,7 +8,6 @@ namespace AssignMnt1 {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("The weather is great!");
-            Console.WriteLine("Feature branch 1 - development 2");
             Console.ReadKey();
         }
     }
