e615f0e7834405c596d31458f3baba2bd247777a
Jingjing Zhao <JIZO@simcorp.com>
2023-05-25 11:05:57 +0200

    V2

--- CalcCurrentValue/Program.cs
+++ CalcCurrentValue/Program.cs
@@ -10,24 +10,29 @@ namespace CalcCurrentValue {
 
             Console.WriteLine("#### Input the basic information for the trade below ####");
             Console.WriteLine("Nominal Amount:");
-            String userInput = Console.ReadLine();
-            int nominal = int.Parse(userInput);
+           
+            int nominal = 0;
+            if (!Int32.TryParse(Console.ReadLine(), out nominal)) {
+                Console.WriteLine("Norminal amount need to be an integer number, try again:");
+                nominal = int.Parse(Console.ReadLine());
+            }
 
             Console.WriteLine("Buy or Sell?");          
-            TransactionType BorS =(TransactionType)Enum.Parse(typeof(TransactionType), Console.ReadLine());
-            int sign = BorS == TransactionType.Buy ? 1 : -1;            
+            TransactionType BorS =(TransactionType)Enum.Parse(typeof(TransactionType), Console.ReadLine(),true);
+            int sign = BorS == TransactionType.Buy ? 1 : -1;
 
             Console.WriteLine("Agreed Price:");
             var price = float.Parse(Console.ReadLine());
 
+            //Calculate current value
             var cv = sign * nominal * price;
-            Console.WriteLine($"Current Value is: {cv}");
-
+            Console.WriteLine($"{BorS.ToString()} "+$"{nominal} trade "+$"with agree price = {price} means you will have to pay "+$"{cv} for the current value.");
+        
             Console.ReadKey();
         }
 
-
         private enum TransactionType { Buy, Sell };
+        
     }
 
 }
